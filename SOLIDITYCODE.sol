517e110aa983a7e47efc0b2e7b0d26c75b79968b
AbhishekRaj7 <abhishekshandilya811@gmail.com>
2024-06-29 11:14:03 +0530

    Update SOLIDITYCODE.sol

--- SOLIDITYCODE.sol
+++ SOLIDITYCODE.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ExampleContract {
+    address public owner;
+    uint256 public value;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+
+    function setValue(uint256 _value) public onlyOwner {
+        require(_value > 0, "Value must be greater than zero");
+        value = _value;
+    }
+
+    function assertValue(uint256 _value) public view {
+        assert(_value == value);
+    }
+
+    function revertIfNotOwner() public view {
+        if (msg.sender != owner) {
+            revert("Caller is not the owner");
+        }
+    }
+}
 
